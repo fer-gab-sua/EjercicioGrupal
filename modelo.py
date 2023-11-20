9d530b7dde43fe02c2609ada22434585531d128a
fer-gab-sua <fer.gab.sua@gmail.com>
2023-11-20 11:52:30 -0300

    1.6.3
    se esta trabajando en el modelo

--- modelo.py
+++ modelo.py
@@ -49,123 +49,132 @@ class MiBaseDeDatos():
             nombre_base_de_datos (str, optional): _description_. Defaults to "facturacion.db".
         """        
         self.nombre_base_de_datos = nombre_base_de_datos
-        self.conexion = sqlite3.connect(self.nombre_base_de_datos)
+        self.conexion = None
         self.cursor = None
-        self.conectar()
         self.crear_tablas()
+
 
     def conectar(self):
         try:
+            self.conexion = sqlite3.connect(self.nombre_base_de_datos)
             self.conexion.isolation_level = None  # Desactiva el autocommit
             self.cursor = self.conexion.cursor()
             print("---> Conexión abierta")
         except sqlite3.Error as error:
             print(f"Error al conectar a la base de datos: {error}")
 
-    def crear_tablas(self):
-        if self.cursor:
-            try:
-                #CREO TABLA - FACTURACION
-                self.cursor.execute("""CREATE TABLE IF NOT EXISTS facturacion (
-                                    fac_int_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    fac_date_fecha DATE,
-                                    fac_int_idconcepto INTEGER,
-                                    fac_bol_monto REAL,
-                                    fac_txt_cuilcliente TEXT
-                                    )
-                                    """)
-                #CREO TABLA  -  CONCEPTOS
-                self.cursor.execute("""CREATE TABLE IF NOT EXISTS conceptos (
-                                    con_int_idconcepto INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    con_txt_descripcion TEXT,
-                                    con_txt_estado  TEXT
-                                    )
-                                    """)
-                #CREO TABLA  -  CATEGORIA_MONOTRIBUTO
-                self.cursor.execute("""CREATE TABLE IF NOT EXISTS categoria_monotributo(
-                                    cat_int_idcategoria INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    cat_txt_descripcion TEXT,
-                                    cat_bol_montotope REAL,
-                                    cat_txt_fecha DATE
-                                    )
-                                    """)
-                #CREO TABLA  -  CONFIG_APP
-                self.cursor.execute("""CREATE TABLE IF NOT EXISTS config_app(
-                                    config_int_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    config_txt_tipo TEXT,
-                                    config_txt_valor TEXT
-                                    )
-                                    """)
-                self.conexion.commit()
-                print("---> Bases validadas/creadas")
-            except sqlite3.Error as error:
-                print(f"Error en la actualización de datos: {error}")
-                self.conexion.rollback()
-
     def desconectar(self):
+        if self.cursor:
+            self.cursor.close()
         if self.conexion:
             self.conexion.close()
             print("---> Conexión cerrada")
 
+
+    def crear_tablas(self):
+        self.conectar()
+        try:
+            #CREO TABLA - FACTURACION
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS facturacion (
+                                fac_int_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                fac_date_fecha DATE,
+                                fac_int_idconcepto INTEGER,
+                                fac_bol_monto REAL,
+                                fac_txt_cuilcliente TEXT
+                                )
+                                """)
+            #CREO TABLA  -  CONCEPTOS
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS conceptos (
+                                con_int_idconcepto INTEGER PRIMARY KEY AUTOINCREMENT,
+                                con_txt_descripcion TEXT,
+                                con_txt_estado  TEXT
+                                )
+                                """)
+            #CREO TABLA  -  CATEGORIA_MONOTRIBUTO
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS categoria_monotributo(
+                                cat_int_idcategoria INTEGER PRIMARY KEY AUTOINCREMENT,
+                                cat_txt_descripcion TEXT,
+                                cat_bol_montotope REAL,
+                                cat_txt_fecha DATE
+                                )
+                                """)
+            #CREO TABLA  -  CONFIG_APP
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS config_app(
+                                config_int_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                config_txt_tipo TEXT,
+                                config_txt_valor TEXT
+                                )
+                                """)
+            self.conexion.commit()
+            print("---> Bases validadas/creadas")
+        except sqlite3.Error as error:
+            print(f"Error en la actualización de datos: {error}")
+            self.conexion.rollback()
+        finally:
+            self.desconectar()
+
+
+
     def carga_datos_iniciales(self):
         """ingresa los datos iniciales de categoria de monotributo, configuracion inicial.
         """  
-        if self.cursor:
-            try:
-                sql = """
-                    INSERT OR IGNORE INTO categoria_monotributo (cat_txt_descripcion, cat_bol_montotope, cat_txt_fecha)
-                    SELECT ?, ?, ?
-                    WHERE NOT EXISTS (SELECT 1 FROM categoria_monotributo WHERE cat_txt_descripcion = ?)
-                """
-                data = ('A',1414762.58,"01/01/2023",'A')
-                self.cursor.execute(sql, data)
+        self.conectar()
+        try:
+            sql = """
+                INSERT OR IGNORE INTO categoria_monotributo (cat_txt_descripcion, cat_bol_montotope, cat_txt_fecha)
+                SELECT ?, ?, ?
+                WHERE NOT EXISTS (SELECT 1 FROM categoria_monotributo WHERE cat_txt_descripcion = ?)
+            """
+            data = ('A',1414762.58,"01/01/2023",'A')
+            self.cursor.execute(sql, data)
 
-                data = ('B',2103025.45,"01/01/2023",'B')
-                self.cursor.execute(sql, data)
+            data = ('B',2103025.45,"01/01/2023",'B')
+            self.cursor.execute(sql, data)
 
-                data = ('C',2944235.6,"01/01/2023",'C')
-                self.cursor.execute(sql, data)
+            data = ('C',2944235.6,"01/01/2023",'C')
+            self.cursor.execute(sql, data)
 
-                data = ('D',3656604.33,"01/01/2023",'D')
-                self.cursor.execute(sql, data)
+            data = ('D',3656604.33,"01/01/2023",'D')
+            self.cursor.execute(sql, data)
 
-                data = ('E',4305799.15,"01/01/2023",'E')
-                self.cursor.execute(sql, data)
+            data = ('E',4305799.15,"01/01/2023",'E')
+            self.cursor.execute(sql, data)
 
-                data = ('F',5382248.94,"01/01/2023",'F')
-                self.cursor.execute(sql, data)
+            data = ('F',5382248.94,"01/01/2023",'F')
+            self.cursor.execute(sql, data)
 
-                data = ('G',6458698.71,"01/01/2023",'G')
-                self.cursor.execute(sql, data)
+            data = ('G',6458698.71,"01/01/2023",'G')
+            self.cursor.execute(sql, data)
 
-                data = ('H',7996484.12,"01/01/2023",'H')
-                self.cursor.execute(sql, data)
+            data = ('H',7996484.12,"01/01/2023",'H')
+            self.cursor.execute(sql, data)
 
-                #ACA ESTOY CARGANDO EN UNA BASE config el tipo color_fondo y el valor #004080
-                sql = """
-                INSERT INTO config_app (config_txt_tipo , config_txt_valor) 
-                SELECT ?,?
-                WHERE NOT EXISTS (SELECT 1 FROM config_app WHERE config_txt_tipo = 'color_fondo')
-                """
-                data = ('color_fondo','#004080')
-                self.cursor.execute(sql, data)
+            #ACA ESTOY CARGANDO EN UNA BASE config el tipo color_fondo y el valor #004080
+            sql = """
+            INSERT INTO config_app (config_txt_tipo , config_txt_valor) 
+            SELECT ?,?
+            WHERE NOT EXISTS (SELECT 1 FROM config_app WHERE config_txt_tipo = 'color_fondo')
+            """
+            data = ('color_fondo','#004080')
+            self.cursor.execute(sql, data)
 
-                #ACA ESTOY CARGANDO EN UNA BASE config el tipo 'link_afip' y el valor 'https://www.afip.gob.ar/monotributo/categorias.asp'
-                sql = """
-                INSERT INTO config_app (config_txt_tipo , config_txt_valor) 
-                SELECT ?,?
-                WHERE NOT EXISTS (SELECT 1 FROM config_app WHERE config_txt_tipo = 'link_afip')
-                """
-                data = ('link_afip','https://www.afip.gob.ar/monotributo/categorias.asp')
-                self.cursor.execute(sql, data)
+            #ACA ESTOY CARGANDO EN UNA BASE config el tipo 'link_afip' y el valor 'https://www.afip.gob.ar/monotributo/categorias.asp'
+            sql = """
+            INSERT INTO config_app (config_txt_tipo , config_txt_valor) 
+            SELECT ?,?
+            WHERE NOT EXISTS (SELECT 1 FROM config_app WHERE config_txt_tipo = 'link_afip')
+            """
+            data = ('link_afip','https://www.afip.gob.ar/monotributo/categorias.asp')
+            self.cursor.execute(sql, data)
 
 
-                self.conexion.commit()
-                print("datos cargados correctamente")
-            except sqlite3.Error as error:
-                print(f"Error en la carga inicial: {error}")
-                self.conexion.rollback()
-
+            self.conexion.commit()
+            print("datos cargados correctamente")
+        except sqlite3.Error as error:
+            print(f"Error en la carga inicial: {error}")
+            self.conexion.rollback()
+        finally:
+            self.desconectar()
 
     def return_config(self,parametro):
         """CONSULA EN LA BASE DE DATOS, DEPENDIENDO DEL PARAMETRO EN LA TABLA CONFIGURACION
@@ -176,21 +185,23 @@ class MiBaseDeDatos():
         Returns:
             str: config_txt_valor 
         """            
-        if self.cursor:
-                        try:
-                            sql = """
-                                SELECT config_txt_valor 
-                                FROM config_app 
-                                WHERE config_txt_tipo = ?
-                            """
-                            data = (parametro,)
-                            self.cursor.execute(sql, data)
-                            retorno = (self.cursor.fetchall()[0][0])
-                            return retorno
-                        except sqlite3.Error as error:
-                            print(f"Error a retornar {parametro}: {error}")
-                            self.conexion.rollback()
-                        
+        self.conectar()
+        try:
+            sql = """
+                SELECT config_txt_valor 
+                FROM config_app 
+                WHERE config_txt_tipo = ?
+            """
+            data = (parametro,)
+            self.cursor.execute(sql, data)
+            retorno = (self.cursor.fetchall()[0][0])
+            return retorno
+        except sqlite3.Error as error:
+            print(f"Error a retornar {parametro}: {error}")
+            self.conexion.rollback()
+        finally:
+            self.desconectar()
+
     def grabar_config(self,config_txt_tipo, cinfig_txt_valor):
         """GRABA UNA NUEVA CONFIGURACION
 
@@ -198,107 +209,115 @@ class MiBaseDeDatos():
             config_txt_tipo (STR): 'color_fondo','link_afip'
             cinfig_txt_valor (STR): nuevo_valor
         """            
-        if self.cursor:
-                    try:
-                        sql = """
-                            UPDATE config_app
-                            SET config_txt_valor = ?
-                            WHERE config_txt_tipo = ?
-                        """
-                        data = (cinfig_txt_valor,config_txt_tipo,)
-                        self.cursor.execute(sql, data)
-                        print(f"-->configuracion {config_txt_tipo}grabada exitosamente")
-                    except sqlite3.Error as error:
-                        print(f"Error a retornar {cinfig_txt_valor,config_txt_tipo}: {error}")
-                        self.conexion.rollback()
+        self.conectar()
+        try:
+            sql = """
+                UPDATE config_app
+                SET config_txt_valor = ?
+                WHERE config_txt_tipo = ?
+            """
+            data = (cinfig_txt_valor,config_txt_tipo,)
+            self.cursor.execute(sql, data)
+            print(f"-->configuracion {config_txt_tipo}grabada exitosamente")
+        except sqlite3.Error as error:
+            print(f"Error a retornar {cinfig_txt_valor,config_txt_tipo}: {error}")
+            self.conexion.rollback()
+        finally:
+            self.desconectar()
 
     def trae_conceptos(self):
-        if self.cursor:
-            try:
-                sql = """
-                    SELECT *
-                    FROM conceptos
-                    WHERE con_txt_estado = 'activo'
-                    """ 
-                self.cursor.execute(sql)
-                retorno = (self.cursor.fetchall())
-                print(retorno)
-                return retorno
-            except sqlite3.Error as error:
-                print(f"Error a retornar conceptos: {error}")
-                self.conexion.rollback()
+        self.conectar()
+        try:
+            sql = """
+                SELECT *
+                FROM conceptos
+                WHERE con_txt_estado = 'activo'
+                """ 
+            self.cursor.execute(sql)
+            retorno = (self.cursor.fetchall())
+            print(retorno)
+            return retorno
+        except sqlite3.Error as error:
+            print(f"Error a retornar conceptos: {error}")
+            self.conexion.rollback()
+        finally:
+            self.desconectar()
         
     def agrega_concepto(self,concepto):
-        if self.cursor:
-            try:
-                sql = """
-                    INSERT INTO conceptos (con_txt_descripcion , con_txt_estado) 
-                    SELECT ?,?
-                    WHERE NOT EXISTS (SELECT 1 FROM conceptos WHERE con_txt_descripcion = ?)
-                """
-                data = [concepto,"activo",concepto]
-                self.cursor.execute(sql,data)
-            except sqlite3.Error as error:
-                        print(f"Error a grabar {concepto}: {error}")
-                        self.conexion.rollback()
+        self.conectar()
+        try:
+            sql = """
+                INSERT INTO conceptos (con_txt_descripcion , con_txt_estado) 
+                SELECT ?,?
+                WHERE NOT EXISTS (SELECT 1 FROM conceptos WHERE con_txt_descripcion = ?)
+            """
+            data = [concepto,"activo",concepto]
+            self.cursor.execute(sql,data)
+        except sqlite3.Error as error:
+                    print(f"Error a grabar {concepto}: {error}")
+                    self.conexion.rollback()
+        finally:
+            self.desconectar()
 
     def traer_categorias(self):
-        if self.cursor:
-            try:
-
-
-                sql = """
-                    SELECT *
-                    FROM categoria_monotributo
-                    WHERE cat_txt_fecha = "01/01/2023"
-                    """ 
-                data = ("01/01/2023",)
-                self.cursor.execute(sql)
-                retorno = (self.cursor.fetchall())
-                return retorno
-            except sqlite3.Error as error:
-                print(f"Error a retornar categorias: {error}")
-                self.conexion.rollback()
+        self.conectar()
+        try:
+            sql = """
+                SELECT *
+                FROM categoria_monotributo
+                WHERE cat_txt_fecha = "01/01/2023"
+                """ 
+            data = ("01/01/2023",)
+            self.cursor.execute(sql)
+            retorno = (self.cursor.fetchall())
+            return retorno
+        except sqlite3.Error as error:
+            print(f"Error a retornar categorias: {error}")
+            self.conexion.rollback()
+        finally:
+            self.desconectar()
 
     def cambiar_categorias(self,bloque):
-        if self.cursor:
-            try:
-                for cat in bloque:
-                    print(cat[0])
-                    print(cat[1])
-                    print(cat[2])
-                    sql = """INSERT OR IGNORE INTO categoria_monotributo (cat_txt_descripcion, cat_bol_montotope, cat_txt_fecha)
-                    SELECT ?, ?, ?
-                    """
-                    #WHERE NOT EXISTS (SELECT 1 FROM categoria_monotributo WHERE cat_txt_estado = "activo")
-                    #   """
-                    data = (cat[0],(cat[1]),(cat[2]))
-                    self.cursor.execute(sql,data)
+        self.conectar()
+        try:
+            for cat in bloque:
+                print(cat[0])
+                print(cat[1])
+                print(cat[2])
+                sql = """INSERT OR IGNORE INTO categoria_monotributo (cat_txt_descripcion, cat_bol_montotope, cat_txt_fecha)
+                SELECT ?, ?, ?
+                """
+                #WHERE NOT EXISTS (SELECT 1 FROM categoria_monotributo WHERE cat_txt_estado = "activo")
+                #   """
+                data = (cat[0],(cat[1]),(cat[2]))
+                self.cursor.execute(sql,data)
 
-            except sqlite3.Error as error:
-                print(f"Error a retornar categorias: {error}")
-                self.conexion.rollback()
+        except sqlite3.Error as error:
+            print(f"Error a retornar categorias: {error}")
+            self.conexion.rollback()
+        finally:
+            self.desconectar()
 
 class Validador():
-     
-     def valida_concepto (self,valido_concepto):
-          if valido_concepto == "":
-               return "ERROR"
-          
-     def valida_monto(self,texto):
+    
+    def valida_concepto (self,valido_concepto):
+        if valido_concepto == "":
+            return "ERROR"
+        
+    def valida_monto(self,texto):
         patron = r"^\d+(\.\d+)?$" #Valida numeros enteros y decimales.
         cadena =  texto
         if re.match (patron,cadena):
             return "OK"
         else:
-             return "ERROR"
-     
+            return "ERROR"
+    
 
-     
-     def valida_fecha(self,valido_fecha):
-          if valido_fecha == "":
-               return "ERROR"
-          
+    
+    def valida_fecha(self,valido_fecha):
+        if valido_fecha == "":
+            return "ERROR"
+
 
 #    def modificar_factura(self,factura_id, nueva_fecha, nuevo_id_concepto, nuevo_monto, nuevo_cuil_cliente):
 #        """MODIFICA UNA FACTURA EXISTENTE EN LA BASE DE DATOS
@@ -340,7 +359,6 @@ if __name__ == "__main__":
     # Uso de la clase Factura
     mibase = MiBaseDeDatos()
     mibase.carga_datos_iniciales()
-    print("si le pido a la funcion link_afip, me devuelve:" , mibase.return_config('link_afip'))
-    print("si le pido a la funcion color_fondo, me devuelve:" , mibase.return_config('color_fondo'))
-
+    #print("si le pido a la funcion link_afip, me devuelve:" , mibase.return_config('link_afip'))
+    #print("si le pido a la funcion color_fondo, me devuelve:" , mibase.return_config('color_fondo'))
 
