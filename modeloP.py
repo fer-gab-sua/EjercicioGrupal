45a299e6b34fb2b75937aaddd18e86186263492b
fer-gab-sua <fer.gab.sua@gmail.com>
2023-11-21 18:18:22 -0300

    1.6.4

--- modeloP.py
+++ modeloP.py
@@ -25,13 +25,25 @@ class MiBaseDeDatosNw():
         pass
 
 
-    def alta(self, fecha, concepto, monto, instante=None):
+    def cargar_datos(self, fecha, concepto, monto, instante=None):
         factura = Factura()
         factura.fac_date_fecha = fecha
         factura.fac_txt_concepto = concepto
         factura.fac_bol_monto = monto
         factura.instante = instante or datetime.now()  
         factura.save()
+
+    def borrar_datos(self,borrar):
+        obj_borrar = Factura.get(Factura.get(Factura.fac_int_id==borrar))
+        obj_borrar.delete_instance()
+
+
+    def actualizar_datos(self,act_fecha,act_concepto,act_monto,id_factura, instante = None):
+        factura_mod = Factura.update(fac_date_fecha=act_fecha , fac_txt_concepto = act_concepto, fac_bol_monto = act_monto).where(Factura.fac_int_id==id_factura)
+        factura_mod.execute()
+
+
+
 
     def actualizar_treeview(self):
         datos = []
@@ -44,3 +56,5 @@ class MiBaseDeDatosNw():
 
 mibase = MiBaseDeDatosNw()
 print(mibase.actualizar_treeview())
+
+mibase.actualizar_datos("fecha","concepto",20.3,4)
